b3c1f2fffd6d18f4ec0f695546719cb6cfc375c7
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-31 15:02:51 +0530

    [Raj] Added:- The code to find max of three float value

--- MaximumProblemUsingGenerics/MaxNumber.cs
+++ MaximumProblemUsingGenerics/MaxNumber.cs
@@ -8,7 +8,7 @@ namespace MaximumProblemUsingGenerics
 {
     public class MaxNumber
     {
-        //Method to compare three integer to get the maximum values(UC1)
+        //Method to compare three integer numbers to get the maximum values(UC1)
         public static int MaxIntNumThree(int firstValue, int secondValue, int thirdValue)
         {
             if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
@@ -20,6 +20,24 @@ namespace MaximumProblemUsingGenerics
                 return secondValue;
             }
             else if(thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return default;
+        }
+
+        //Method to compare three float numbers to get the maximum values(UC2)
+        public static float MaxFloatNumThree(float firstValue, float secondValue, float thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(secondValue) >= 0)
             {
                 return thirdValue;
             }
--- MaximumProblemUsingGenerics/Program.cs
+++ MaximumProblemUsingGenerics/Program.cs
@@ -13,16 +13,38 @@ namespace MaximumProblemUsingGenerics
             //Printing welcome message
             Console.WriteLine("Welcome to the finding maximum number program using generics");
 
-            //Finding maximum value out of three integer(UC1)
-            Console.Write("Enter First Number : ");
-            int firstValue = int.Parse(Console.ReadLine());
-            Console.Write("Enter Second Number : ");
-            int secondValue = int.Parse(Console.ReadLine());
-            Console.Write("Enter Third Number : ");
-            int thirdValue = int.Parse(Console.ReadLine());
-            int resultNum = MaxNumber.MaxIntNumThree(firstValue, secondValue, thirdValue);
-            Console.WriteLine("{0} is maximum value from first: {1} second: {2} third: {3}", resultNum, firstValue, secondValue, thirdValue); 
-            Console.ReadLine();
+            while(true)
+            {
+                Console.WriteLine("1: Find Max Of Three Int Number"+
+                             "\n2: Find Max Of Three Float Number"+
+                             "\n3: Exit"
+                            );
+                Console.Write("Enter A Choice From Above : ");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        //Finding maximum value out of three integer(UC1)
+                        int fIntValue = 50, sIntValue = 20, tIntValue = 10;
+                        int resIntNum = MaxNumber.MaxIntNumThree(fIntValue, sIntValue, tIntValue);
+                        Console.WriteLine("{0} is maximum value from first: {1} second: {2} third: {3}", resIntNum, fIntValue, sIntValue, tIntValue);
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        //Finding maximum value out of three float numbers(UC2)
+                        float fstFtVal = 2.2f, secFtVal = 3.4f, tFtVal = 3.2f;
+                        float resFtNum = MaxNumber.MaxFloatNumThree(fstFtVal, secFtVal, tFtVal);
+                        Console.WriteLine("{0} is maximum value from first: {1} second: {2} third: {3}", resFtNum, fstFtVal, secFtVal, tFtVal);
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Choose a right option");
+                        break;
+                }
+            }
         }
     }
 }
