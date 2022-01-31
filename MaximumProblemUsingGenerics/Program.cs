272232b5dbe444f5ded06c033b7970468def3701
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-31 14:39:09 +0530

    [Raj] Added:- The code to find max of three int value

--- MaximumProblemUsingGenerics/Program.cs
+++ MaximumProblemUsingGenerics/Program.cs
@@ -12,6 +12,16 @@ namespace MaximumProblemUsingGenerics
         {
             //Printing welcome message
             Console.WriteLine("Welcome to the finding maximum number program using generics");
+
+            //Finding maximum value out of three integer(UC1)
+            Console.Write("Enter First Number : ");
+            int firstValue = int.Parse(Console.ReadLine());
+            Console.Write("Enter Second Number : ");
+            int secondValue = int.Parse(Console.ReadLine());
+            Console.Write("Enter Third Number : ");
+            int thirdValue = int.Parse(Console.ReadLine());
+            int resultNum = MaxNumber.MaxIntNumThree(firstValue, secondValue, thirdValue);
+            Console.WriteLine("{0} is maximum value from first: {1} second: {2} third: {3}", resultNum, firstValue, secondValue, thirdValue); 
             Console.ReadLine();
         }
     }
