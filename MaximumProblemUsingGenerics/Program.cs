bc03f05be80fb52f9d174b40ff55b281390eda53
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-31 13:58:08 +0530

    [Raj] Added:- The welcome message in main program

---
+++ MaximumProblemUsingGenerics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumProblemUsingGenerics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Printing welcome message
+            Console.WriteLine("Welcome to the finding maximum number program using generics");
+            Console.ReadLine();
+        }
+    }
+}
